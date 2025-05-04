e97375d9aa577ed2444c207fe2eb6c70e38aec1b
Luke3470 <ljcadman@outlook.com>
2025-05-04 23:00:39 +0100

    Changes to Graph are required need to make an average per airline then also add min and max time you can do it

--- src/UI/Graph/ChooChooPlaneGraphController.java
+++ src/UI/Graph/ChooChooPlaneGraphController.java
@@ -1,12 +1,10 @@
 package UI.Graph;
 
 import UI.main.ChooChooPlaneView;
-import org.sqlite.core.DB;
-
 public class ChooChooPlaneGraphController {
 
-    public ChooChooPlaneGraphController(String DBurl, String[][] filters, String graphTitle, String xLabel, String yLabel, ChooChooPlaneView error){
-        ChooChooPlaneGraphModel model = new ChooChooPlaneGraphModel(DBurl);
-        ChooChooPlaneGraphView view = new ChooChooPlaneGraphView(graphTitle,yLabel,yLabel,model.getGraphData(filters),error);
+    public ChooChooPlaneGraphController(String dbURL, String[][] filters, String graphTitle, String xLabel, String yLabel, ChooChooPlaneView error){
+        ChooChooPlaneGraphModel model = new ChooChooPlaneGraphModel(dbURL);
+        ChooChooPlaneGraphView view = new ChooChooPlaneGraphView(graphTitle,xLabel,yLabel,model.getGraphData(filters),error);
     }
 }
